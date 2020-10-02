ca50bb4ba65aff32841aae5005d881c657904b35
Dr. Huibregtse <sarah@hubrex.com>
2020-10-02 15:38:12 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Sarah J. Huibregtse, PhD', // Title for your website.
-    tagline: 'On a mission to promote\nComputing Education & Research, Health & Well Being, and Universal Learning Design.',
+    tagline: 'On a mission to promote Computing Education & Research,\nHealth & Well Being, and Universal Learning Design.',
     profilePhoto: 'img/profile.png',
     url: 'https://sjhuibregtse.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
